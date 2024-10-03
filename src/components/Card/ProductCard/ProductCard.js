a400da83c072abf57a6de57f9a22df96bd45b5ae
Artem <blbo@yandex.ru>
2024-10-03 13:44:13 +0400

    0.1.9
    Верстка карточки товаров.

--- src/components/Card/ProductCard/ProductCard.js
+++ src/components/Card/ProductCard/ProductCard.js
@@ -6,107 +6,119 @@ function ProductCard() {
     return (
         <>
             <div className="card__card-product">
-                <span>Масло ПРОСТОКВАШИНО сливочное в/с 82% фольга без змж, Россия, 180 г</span>
-                <div className="card__box-button">
-                    <span className="card__product-article">арт. 371431</span>
 
-                    <div className="card__box-rating">
-                            <span className="product__star product__star_on"></span>
-                            <span className="product__star product__star_on"></span>
-                            <span className="product__star product__star_on"></span>
-                            <span className="product__star product__star_on"></span>
-                            <span className="product__star"></span>
+                <div className="card__header">
+                    <span className="card__name">Масло ПРОСТОКВАШИНО сливочное в/с 82% фольга без змж, Россия, 180 г</span>
 
-                            <span className="card__rating-text"><a href="/">3 отзыва</a></span>
+                    <div className="card__box-button">
+                        <span className="card__product-article">арт. 371431</span>
 
-                    </div>
+                        <div className="card__box-rating">
+                                <span className="product__star product__star_on"></span>
+                                <span className="product__star product__star_on"></span>
+                                <span className="product__star product__star_on"></span>
+                                <span className="product__star product__star_on"></span>
+                                <span className="product__star"></span>
 
-                    <button className="card__button card__button-share" type="button">Поделиться</button>
-                    <button className="card__button card__button-favorites" type="button">В избранное</button>
-                </div>
+                                <span className="card__rating-text"><a href="/">3 отзыва</a></span>
 
-                <div className="card__box-images">
-                    <div className="card__box-small">
-                        <div className="card__small">
-                            <img src={imgProduct} className="card__images card__images_small" alt="Изображение продукта"/>
                         </div>
-                        <div className="card__small">
-                            <img src={imgProduct} className="card__images card__images_small" alt="Изображение продукта"/>
-                        </div>
-                        <div className="card__small">
-                            <img src={imgProduct} className="card__images card__images_small" alt="Изображение продукта"/>
-                        </div>
-                        <div className="card__small">
-                            <img src={imgProduct} className="card__images card__images_small" alt="Изображение продукта"/>
-                        </div>
-                    </div>
 
-
-                    <div className="card__big">
-                        <img src={imgProduct} className="card__images card__images_big" alt="Изображение продукта" />
-                        <span className="card__sales">-50%</span>
+                        <button className="card__button card__button-share" type="button">Поделиться</button>
+                        <button className="card__button card__button-favorites" type="button">В избранное</button>
                     </div>
                 </div>
 
-                <div className="card__box-information">
-                    <div className="card___box-price">
-                        <div className="card___price">
-                            <span className="card___price-nocart">130 ₽</span>
-                            <span className="card___title">Обычная</span>
+                <div className="card__box">
+
+                    <div className="card__box-images">
+                        <div className="card__box-small">
+                            <div className="card__small">
+                                <img src={imgProduct} className="card__images card__images_small" alt="Изображение продукта"/>
+                            </div>
+                            <div className="card__small">
+                                <img src={imgProduct} className="card__images card__images_small" alt="Изображение продукта"/>
+                            </div>
+                            <div className="card__small">
+                                <img src={imgProduct} className="card__images card__images_small" alt="Изображение продукта"/>
+                            </div>
+                            <div className="card__small">
+                                <img src={imgProduct} className="card__images card__images_small" alt="Изображение продукта"/>
+                            </div>
+                            <div className="card__small">
+                            <img src={imgProduct} className="card__images card__images_small" alt="Изображение продукта"/>
+                        </div>
                         </div>
 
-                        <div className="card___price">
-                            <span className="card___price-cart">100 ₽</span>
-                            <span className="card___title">С картой Северяночки</span>
-                        </div>
-                    </div>
-                    
-                    <button className="card__button-buy" type="button">В корзину</button>
-                    <span className="card__bonus"></span>
-                    <button className="card__button-decrease" type="button">Уведомить о снижении цены</button>
 
-                    <div className="card__box-characteristics">
-                        <div className="card__characteristics">
-                            <span className="card__characteristics-left">Бренд</span>
-                            <span className="card__characteristics-right">ПРОСТОКВАШИНО</span>
-                        </div>
-                        <div className="card__characteristics">
-                            <span className="card__characteristics-left">Страна производителя</span>
-                            <span className="card__characteristics-right">Россия</span>
-                        </div>
-                        <div className="card__characteristics">
-                            <span className="card__characteristics-left">Упаковка</span>
-                            <span className="card__characteristics-right">180 г</span>
+                        <div className="card__big">
+                            <img src={imgProduct} className="card__images card__images_big" alt="Изображение продукта" />
+                            <span className="card__sales">-50%</span>
                         </div>
                     </div>
-                </div>
 
-                <div className="card__similar">
-                    <span>Похожие</span>
-                    <div className="card__box-similar">
-                        <div className="card__similar-product">
-                            <img src={imgProduct} className="card__images card__images_small" alt="Изображение продукта" />
-                            <span>200 ₽</span>
+                    <div className="card__box-information">
+                        <div className="card___box-price">
+                            <div className="card___price">
+                                <span className="card___price-nocart">130 ₽</span>
+                                <span className="card___title">Обычная</span>
+                            </div>
+
+                            <div className="card___price">
+                                <span className="card___price-cart">100 ₽</span>
+                                <span className="card___title">С картой Северяночки</span>
+                            </div>
                         </div>
-                    
-                    <div className="card__similar-product">
-                        <img src={imgProduct} className="card__images card__images_small" alt="Изображение продукта" />
-                        <span>300 ₽</span>
+                        
+                        <button className="card__button-buy" type="button">В корзину</button>
+                        <span className="card__bonus">Вы получаете 10 бонусов</span>
+                        <button className="card__button-decrease" type="button">Уведомить о снижении цены</button>
+
+                        <div className="card__box-characteristics">
+                            <div className="card__characteristics">
+                                <span className="card__characteristics-left">Бренд</span>
+                                <span className="card__characteristics-right">ПРОСТОКВАШИНО</span>
+                            </div>
+                            <div className="card__characteristics">
+                                <span className="card__characteristics-left">Страна производителя</span>
+                                <span className="card__characteristics-right">Россия</span>
+                            </div>
+                            <div className="card__characteristics">
+                                <span className="card__characteristics-left">Упаковка</span>
+                                <span className="card__characteristics-right">180 г</span>
+                            </div>
+                        </div>
                     </div>
 
-                    <div className="card__similar-product">
-                        <img src={imgProduct} className="card__images card__images_small" alt="Изображение продукта" />
-                        <span>250 ₽</span>
-                    </div>
+                    <div className="card__similar">
+                        <span>Похожие</span>
+                        <div className="card__box-similar">
 
-                    <div className="card__similar-product">
-                        <img src={imgProduct} className="card__images card__images_small" alt="Изображение продукта" />
-                        <span>210 ₽</span>
-                    </div>
+                            <div className="card__similar-product">
+                                <img src={imgProduct} className="card__images card__images_small" alt="Изображение продукта" />
+                                <span>200 ₽</span>
+                            </div>
+                        
+                            <div className="card__similar-product">
+                                <img src={imgProduct} className="card__images card__images_small" alt="Изображение продукта" />
+                                <span>300 ₽</span>
+                            </div>
 
+                            <div className="card__similar-product">
+                                <img src={imgProduct} className="card__images card__images_small" alt="Изображение продукта" />
+                                <span>250 ₽</span>
+                            </div>
+
+                            <div className="card__similar-product">
+                                <img src={imgProduct} className="card__images card__images_small" alt="Изображение продукта" />
+                                <span>210 ₽</span>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
             </div>
+
         </>
     );
 };
